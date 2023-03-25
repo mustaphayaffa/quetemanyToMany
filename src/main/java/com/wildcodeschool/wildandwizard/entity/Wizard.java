aea653e32ba0f1c40ccf78d368a63eb17c12fef4
BA\_MUSTAPHA <pro.mustapha@gmail.com>
2023-03-25 14:33:28 +0100

    fianal

---
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -0,0 +1,103 @@
+package com.wildcodeschool.wildandwizard.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Wizard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthday;
+    private String birthPlace;
+    private String biography;
+    private boolean muggle;
+
+    // rajout de l'annotation manytomany
+    @ManyToMany
+    @JoinTable(name = "wizard_courses",
+            joinColumns = @JoinColumn(name = "wizard_id"),
+            inverseJoinColumns = @JoinColumn(name = "courses_id"))
+    private List<Course> courses = new ArrayList<>();
+    //
+
+    public Wizard() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    // on a rajouter les getteurs et setteurs
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public List<Course> getCourses() {
+        return courses;
+    }
+    //
+    
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBirthPlace() {
+        return birthPlace;
+    }
+
+    public void setBirthPlace(String birthPlace) {
+        this.birthPlace = birthPlace;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public boolean isMuggle() {
+        return muggle;
+    }
+
+    public void setMuggle(boolean muggle) {
+        this.muggle = muggle;
+    }
+}
